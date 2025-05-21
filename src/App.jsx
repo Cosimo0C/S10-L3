847d07f7e31354a144bd3059372a776ece59a274
Cosimo0C <cosimocrupi01@gmail.com>
2025-05-21 15:47:15 +0200

    S10-L3

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Hero from "./component/Hero";
+import MyFooter from "./component/MyFooter";
+import MyNavbar from "./component/MyNavbar";
+import MyFilm from "./component/MyFilm";
+function App() {
+  return (
+    <div className="bg-dark">
+      <MyNavbar />
+      <Hero />
+      <MyFilm film="Thor" />
+      <MyFilm film="Batman" />
+      <MyFilm film="Superman" />
+      <MyFooter />
+    </div>
+  );
+}
+export default App;
---
+++ src/component/MyFilm.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import { Container, Row, Col, Spinner, Card, CardImg } from "react-bootstrap";
+
+class MyFilm extends Component {
+  state = {
+    films: [],
+    isLoading: false,
+  };
+  componentDidMount = async () => {
+    this.setState({ isLoading: true });
+
+    try {
+      let resp = await fetch(`http://www.omdbapi.com/?s=${this.props.film}&apikey=c2ca027b`);
+      if (resp.ok) {
+        const films = await resp.json();
+        this.setState({ films: films.Search });
+      } else {
+        throw new Error(`Errore nel reperimento dei film`, resp.status);
+      }
+    } catch (error) {
+      console.log(`error`, error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid className="bg-dark d-flex flex-column justify-content-center ms-2">
+        <h3 className=" text-white">{this.props.film}</h3>
+
+        <Row>
+          {this.state.isLoading && <Spinner animation="border" variant="danger" />}
+          {this.state.films.slice(0, 6).map((film) => (
+            <Col className="p-0" xs={6} md={4} lg={2} key={film.imdbID}>
+              <Card.Img className="p-2" src={film.Poster} />
+            </Col>
+          ))}
+          <Card.Body></Card.Body>
+        </Row>
+      </Container>
+    );
+  }
+}
+export default MyFilm;
